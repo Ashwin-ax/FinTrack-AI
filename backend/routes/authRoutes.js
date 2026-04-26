6846754b8cfed3d84287eb48480a6a59a7ff806a
Ashwin <ashwinpatil394@gmail.com>
2026-04-26 17:26:49 +0530

    Fixed logout bug

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,12 +2,23 @@ const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
+
 router.get("/verify", protect, (req, res) => {
   res.json({ success: true });
 });
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
 
-module.exports = router;
+router.post("/logout", (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    expires: new Date(0), // 🔥 deletes cookie
+  });
+
+  res.json({ message: "Logged out successfully" });
+});
 
 module.exports = router;
